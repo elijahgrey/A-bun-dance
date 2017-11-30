60b7ac328dac5af462fdf11681640670193ea07f
elijahgrey <34036274+elijahgrey@users.noreply.github.com>
2017-11-30 14:14:07 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     dp = updater.dispatcher
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text, hello))
+    dp.add_handler(MessageHandler('hello', hello))
     
 
     # Start the webhook
