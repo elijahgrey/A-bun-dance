a7aa7f96dcf6a273510c873ed7b8f5fc76b7e044
elijahgrey <34036274+elijahgrey@users.noreply.github.com>
2017-11-30 16:20:33 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -17,7 +17,8 @@ def hello(bot, update):
         bot.sendMessage(chat_id=update.message.chat_id, text='Sup '+update.message.from_user.first_name)
         
 def who(bot, update):
-    if update.effective_message.text.lower() == "who":
+    string = update.effective_message.text
+    if string.lower() == "who":
         bot.sendMessage("VoilÃ ! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate. This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation of a by-gone vexation, stands vivified, and has vowed to vanquish these venal and virulent vermin van-guarding vice and vouchsafing the violently vicious and voracious violation of volition. The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous. Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it is my very good honor to meet you and you may call me V. And you would be?")
         
 
