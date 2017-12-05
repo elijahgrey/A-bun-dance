ea7be297c04faba58c033b7d90085c465d06c382
elijahgrey <34036274+elijahgrey@users.noreply.github.com>
2017-12-05 22:42:07 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -16,11 +16,10 @@ def hello(bot, update):
     if string.upper()[:3] == "HI ":
         bot.sendMessage(chat_id=update.message.chat_id, text='Sup '+update.message.from_user.first_name)
         
-def who(bot, update):
-    string = update.effective_message.text
-    if string.upper() == "WHO":
-        bot.sendMessage(text='VoilÃ ! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate. This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation of a by-gone vexation, stands vivified, and has vowed to vanquish these venal and virulent vermin van-guarding vice and vouchsafing the violently vicious and voracious violation of volition. The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous. Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it is my very good honor to meet you and you may call me V. And you would be?')
-        
+def printme( str ):
+   "This prints a passed string into this function"
+   print str
+   return
 
 
 if __name__ == "__main__":
