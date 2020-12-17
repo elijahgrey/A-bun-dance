e10a005ca44d0939750ba1d2869fba4279f247dc
elijahgrey <34036274+elijahgrey@users.noreply.github.com>
2020-12-17 23:17:08 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -1,68 +1,58 @@
 import logging
-import os
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import requests
-import re
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    return url
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-def bop(bot, update):
-    url = get_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chatid, photo=url)
-    
+logger = logging.getLogger(__name__)
+TOKEN = '452290636:AAEAGjoxOjtyZqg8vlNeVn74yxfzi_unhUQ'
 
-chat_id = update.message.chat_id
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-def start(bot, update):
-    update.effective_message.reply_text("Sup bruh")
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-def hello(bot, update):
-    chatid = update.message.chat.id
-    string = update.effective_message.text
-    if string.upper() == "HI":
-        bot.sendMessage(chat_id=update.message.chat_id, text='Sup '+update.message.from_user.first_name)
-    if string.upper()[:3] == "HI ":
-        bot.sendMessage(chat_id=update.message.chat_id, text='Sup '+update.message.from_user.first_name)
-        
-        
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 def main():
-    updater = Updater('452290636:AAEAGjoxOjtyZqg8vlNeVn74yxfzi_unhUQ')
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',bop))
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-if __name__ == "__main__":
-    # Set these variable to the appropriate values
-    TOKEN = "452290636:AAEAGjoxOjtyZqg8vlNeVn74yxfzi_unhUQ"
-    NAME = "testingbotsenabled"
-
-    # Port is given by Heroku
-    PORT = os.environ.get('PORT')
-
-    # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    # Set up the Updater
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-    # Add handlers
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text, hello))
-    dp.add_handler(MessageHandler(Filters.text, who))
-    
-
-    # Start the webhook
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
-    updater.idle()
+if __name__ == '__main__':
+    main(
